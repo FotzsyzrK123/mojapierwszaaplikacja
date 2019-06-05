26b93a7ccff282d04715423c839a627a45d9723b
FotzsyzrK123 <k.koska94@gmail.com>
2019-06-05 14:19:13 +0200

    dziala

---
+++ Pierwsza.java
@@ -0,0 +1,7 @@
+class Pierwsza {
+	public static void main(String[] args) {
+		System.out.println("Ja rządzę światem!");
+		System.out.println("Oczywko!");
+		System.out.println("Pyszne pieczywko!");
+	}
+}
